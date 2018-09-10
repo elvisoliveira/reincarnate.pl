d2baf642fa15490edf55d748d6e6781243878ad8
Elvis Oliveira <elvis.olv@gmail.com>
2018-09-10 11:48:47 +0000

    Syntax check, replaced indentation on tabs by spaces.

--- reincarnate.pl
+++ reincarnate.pl
@@ -42,73 +42,73 @@ my ($str, $agi, $vit, $int, $dex, $luk, $hair_style, $hair_color) =
    (9   , 9   , 9   , 1   , 1   , 1   , 1          , 1          );
 
 sub onUnload {
-	Commands::unregister($command1);
-	Commands::unregister($command2);
-	Plugins::delHooks($plugin);
+    Commands::unregister($command1);
+    Commands::unregister($command2);
+    Plugins::delHooks($plugin);
 }
 sub charSelectScreen {
-	my $email = $config{email};
-	my ($self, $args) = @_;
-	if (!$chars[$charPos]) {
-		$config{char} = $charPos;
-		$newCharName = generateName(int(rand(2)) + 4, int(rand(2)) + 4);
-		$messageSender->sendCharCreate($charPos, $newCharName, $str, $agi, $vit, $int, $dex, $luk, $hair_style, $hair_color);
-		$timeout{'charlogin'}{'time'} = time;
-		$args->{return} = 2;
-		$status = "character created";
-		return;
-	}
-	if ($status eq "start") {
-		$messageSender->sendBanCheck($charID);
-		$messageSender->sendCharDelete($chars[$currentCharPos]{charID}, $email);
-		$timeout{'charlogin'}{'time'} = time;
-		$args->{return} = 2;
-		$status = "character deleted";
-	} elsif ($status eq "character deleted") {
-		$messageSender->sendCharCreate($currentCharPos, $newCharName, $str, $agi, $vit, $int, $dex, $luk, $hair_style, $hair_color);
-		$timeout{'charlogin'}{'time'} = time;
-		$args->{return} = 2;
-		$status = "character created";
-	} elsif ($status eq "character created") {
-		$messageSender->sendCharLogin($currentCharPos);
-		$timeout{'charlogin'}{'time'} = time;
-		$args->{return} = 1;
-		configModify("char", $currentCharPos);
-		$status = "ready";
+    my $email = $config{email};
+    my ($self, $args) = @_;
+    if (!$chars[$charPos]) {
+        $config{char} = $charPos;
+        $newCharName = generateName(int(rand(2)) + 4, int(rand(2)) + 4);
+        $messageSender->sendCharCreate($charPos, $newCharName, $str, $agi, $vit, $int, $dex, $luk, $hair_style, $hair_color);
+        $timeout{'charlogin'}{'time'} = time;
+        $args->{return} = 2;
+        $status = "character created";
+        return;
+    }
+    if ($status eq "start") {
+        $messageSender->sendBanCheck($charID);
+        $messageSender->sendCharDelete($chars[$currentCharPos]{charID}, $email);
+        $timeout{'charlogin'}{'time'} = time;
+        $args->{return} = 2;
+        $status = "character deleted";
+    } elsif ($status eq "character deleted") {
+        $messageSender->sendCharCreate($currentCharPos, $newCharName, $str, $agi, $vit, $int, $dex, $luk, $hair_style, $hair_color);
+        $timeout{'charlogin'}{'time'} = time;
+        $args->{return} = 2;
+        $status = "character created";
+    } elsif ($status eq "character created") {
+        $messageSender->sendCharLogin($currentCharPos);
+        $timeout{'charlogin'}{'time'} = time;
+        $args->{return} = 1;
+        configModify("char", $currentCharPos);
+        $status = "ready";
         # Commands::run("reload macros");
-	}
+    }
 }
 sub reincarnate_command {
-	$currentCharPos = $config{char};
-	$newCharName = generateName(int(rand(2)) + 4, int(rand(2)) + 4);
-	$hair_color = int(rand(12));
-	$hair_style = int(rand(12)) + 2;
-	print "$hair_color, $hair_style\n";
-	print "New name: $newCharName\n";
-	$status = "start";
-	$messageSender->sendQuitToCharSelect();
+    $currentCharPos = $config{char};
+    $newCharName = generateName(int(rand(2)) + 4, int(rand(2)) + 4);
+    $hair_color = int(rand(12));
+    $hair_style = int(rand(12)) + 2;
+    print "$hair_color, $hair_style\n";
+    print "New name: $newCharName\n";
+    $status = "start";
+    $messageSender->sendQuitToCharSelect();
 }
 sub quitToCharSelect_command {
-	$messageSender->sendQuitToCharSelect();
+    $messageSender->sendQuitToCharSelect();
 }
 sub generateName {
-	my $lengthS = shift;
-	return "" if (!$lengthS);
-	my $lengthF = shift;
-	my $surname = "";
-	if ($lengthS) {
-		my $i;
-		for ($i = 0; $i < $lengthS; $i++) {
-			my $syllable = "";
-			while (!$syllable) {
-				my $candidate = $syllables[int(rand(@syllables))];
-				$syllable = $candidate;
-			}
-			$surname .= $syllable;
-		}
-		$surname = ucfirst($surname);
-	}
-	my $name = $surname;
-	return $name;
+    my $lengthS = shift;
+    return "" if (!$lengthS);
+    my $lengthF = shift;
+    my $surname = "";
+    if ($lengthS) {
+        my $i;
+        for ($i = 0; $i < $lengthS; $i++) {
+            my $syllable = "";
+            while (!$syllable) {
+                my $candidate = $syllables[int(rand(@syllables))];
+                $syllable = $candidate;
+            }
+            $surname .= $syllable;
+        }
+        $surname = ucfirst($surname);
+    }
+    my $name = $surname;
+    return $name;
 }
 return 1;
